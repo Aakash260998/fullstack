2193743542c1243df0682d8cc486868cc480af61
Aakash Prasad <akashprasd787@gmail.com>
2021-11-30 13:30:25 +0530

    groovy test class modified

--- backend/src/test/groovy/com/example/demo/service/UserServiceSpec.groovy
+++ backend/src/test/groovy/com/example/demo/service/UserServiceSpec.groovy
@@ -25,17 +25,4 @@ class UserServiceSpec extends Specification {
 		then:
 		  user1==null
 	}	
-//	def "register1" () {
-//		
-//		given:
-//		 User user=new User("Aakash","qwerty","Aakash Prasad","Kolkata",12344567890)
-//		 User user1=new User("Aakash","qwerty","Aakash Prasad","Kolkata",12344567890)
-//		 
-//		when:
-//		def msg= userService.registerUser(user)
-//		def msg2=userService.registerUser(user1)
-//		then:
-//		  msg=="User Registered"
-//		 
-//	}
 }
